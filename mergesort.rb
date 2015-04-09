87e0884ffb6af70fd0bfa6e20bb751dad2a2738b
Austin Kovach <austinkovach@austinkovac-mba.local>
2015-04-09 01:41:04 -0500

    Really ugly solution

--- mergesort.rb
+++ mergesort.rb
@@ -1,24 +1,44 @@
-def merge(array, output = [])
-	print "#{array}\n"
-
+def merge(array)
+	puts "\n\nArray: #{array}"
+	output = []
 	return array if array.size <= 1
 
 	if array.size % 2 == 0
 		then mid = (array.size/2) - 1
 		left = array[0..mid]
 		right = array[(mid + 1)..-1]
-		print "mid: #{mid} left: #{left} right: #{right}\n"
+		#print "mid: #{mid} left: #{left} right: #{right}\n"
 	else
 		mid = (array.size/2)
 		left = array[0..mid]
 		right = array[(mid + 1)..-1]
-		print "mid: #{mid} left: #{left} right: #{right}\n"
+		#print "mid: #{mid} left: #{left} right: #{right}\n"
 	end
 
-	left[0] > right[0] ? (output << left[0]) : (output << right[0])
-	merge(left[1..-1], output)
-	merge(right[1..-1], output)
+	r = merge(right)
+	l = merge(left)
+	
+	stop = (l.size + r.size)
+	puts "l: #{l}\nr: #{r}"
+	puts "stop: (#{l.size} + #{r.size}) = #{stop}"
+
+	until output.size == (stop) do
+		if r == []
+			then output << l[0]
+			l = l[1..-1]
+		elsif l == []
+			then output << r[0]
+			r = r[1..-1]
+		elsif l[0] < r[0]
+			then output << l[0]
+			l = l[1..-1]
+		elsif r[0] < l[0]
+			then output << r[0]
+			r = r[1..-1]
+		end
+	end
+	puts "output: #{output}\r\n\r\n"
 	return output
 end
 
-print merge([5, 2, 7, 3, 0, 1, 4, 9, 6, 8])
+merge([5, 2, 7, 3, 0, 1, 4, 9, 6, 8])

9a2c055232312a3c5fcee28be179262e44caafbf
Austin Kovach <austinkovach@austinkovac-mba.attlocal.net>
2015-04-11 03:47:33 -0500

    Reworked merging logic - now works

--- mergesort.rb
+++ mergesort.rb
@@ -1,44 +1,72 @@
 def merge(array)
-	puts "\n\nArray: #{array}"
 	output = []
-	return array if array.size <= 1
+	return array if array.size == 1
+	mid = array.length/2
 
-	if array.size % 2 == 0
-		then mid = (array.size/2) - 1
-		left = array[0..mid]
-		right = array[(mid + 1)..-1]
-		#print "mid: #{mid} left: #{left} right: #{right}\n"
-	else
-		mid = (array.size/2)
-		left = array[0..mid]
-		right = array[(mid + 1)..-1]
-		#print "mid: #{mid} left: #{left} right: #{right}\n"
-	end
+	right = array[0..(mid - 1)]
+	left = array[(mid)..-1]
 
 	r = merge(right)
 	l = merge(left)
 	
 	stop = (l.size + r.size)
-	puts "l: #{l}\nr: #{r}"
-	puts "stop: (#{l.size} + #{r.size}) = #{stop}"
 
 	until output.size == (stop) do
-		if r == []
-			then output << l[0]
-			l = l[1..-1]
-		elsif l == []
-			then output << r[0]
-			r = r[1..-1]
-		elsif l[0] < r[0]
-			then output << l[0]
-			l = l[1..-1]
-		elsif r[0] < l[0]
-			then output << r[0]
-			r = r[1..-1]
+
+		if r == [] || l == []
+			if r == []
+				then output << l[0]
+				l = l[1..-1]
+			elsif l == []
+				then output << r[0]
+				r = r[1..-1]
+			end
+
+		elsif l[0] <= r[0] || r[0] <= l[0]
+			if l[0] <= r[0]
+				then output << l[0]
+				l = l[1..-1]
+			else 
+				 output << r[0]
+				r = r[1..-1]
+			end
 		end
 	end
-	puts "output: #{output}\r\n\r\n"
-	return output
+	output
 end
 
-merge([5, 2, 7, 3, 0, 1, 4, 9, 6, 8])
+
+
+
+
+=begin
+
+		if r == [] || l == []
+			if r == []
+				then output << l[0]
+				l = l[1..-1]
+			elsif l == []
+				then output << r[0]
+				r = r[1..-1]
+			end
+		elsif l[0] < r[0] || r[0] < l[0]
+			if l[0] < r[0]
+				then output << l[0]
+				l = l[1..-1]
+			elsif r[0] < l[0]
+				then output << r[0]
+				r = r[1..-1]
+			end
+		end
+	end
+	output
+end
+=end
+
+input = []
+100.times {input << rand(100) + 1}
+
+print input
+puts "\nsorted: \n"
+
+p merge(input)
